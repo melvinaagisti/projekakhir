3d2822a7dd0d9e321001e6591204979db7c48a03
melvinaagisti <melvinaagisti16@gmail.com>
2022-04-08 21:51:48 +0700

    new resorce

---
+++ database/migrations/2022_03_27_213012_create_supliers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suprilers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('no hp')->unique();
+            $table->string('alamat')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
